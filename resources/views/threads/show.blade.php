d73e80cb861a323cc3061667015936f50d00f110
Vineeth Vijayan <vineeth030@gmail.com>
2017-05-22 17:48:06 +0530

    chapther 7.5

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -26,5 +26,21 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form method="POST" action="{{ $thread->path() . "/replies" }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" id="body" placeholder="Have something to say?" rows="5" class="form-control"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-default">Post</button>
+                </form>
+            </div>
+        </div>
+        @else
+            <p class="text-center">Please <a href="{{ route("login") }}">sign in</a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
